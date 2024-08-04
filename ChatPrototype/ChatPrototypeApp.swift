c1afe6a780bd801607c213a39102e616c43da24c
Dorian <61510923+dodoleouf@users.noreply.github.com>
2024-08-04 16:23:47 +0300

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Dorian Emenir on 04/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
